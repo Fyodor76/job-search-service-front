74212ac06671559648c0b3e9da4dd94336781fb5
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-11-17 00:41:46 +0300

    удален лишний импорт

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,22 +4,17 @@ import block from "bem-cn-lite";
 import Menu from "./Menu/Menu";
 import Logotype from "./Logotype/Logotype";
 import BlockLogin from "./BlockLogin/BlockLogin";
-import { checkAuth } from "@/helpers/auth";
 
 const b = block("header");
 
-const Header: React.FC = async () => {
-  const { isAuth } = await checkAuth();
-
-  console.log(isAuth, 'isAuth')
-
+const Header: React.FC = () => {
   return (
     <header className={cn(b())}>
       <div className={b("container")}>
         <Logotype className={b("logotype")} />
-        <Menu className={b("menu")}/>
+        <Menu className={b("menu")} />
       </div>
-      <BlockLogin isAuth={isAuth}/>
+      <BlockLogin />
     </header>
   );
 };
