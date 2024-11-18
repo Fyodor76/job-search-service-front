528172a6e706459f8e2e359b83d4e044c73f3a1c
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-11-19 02:08:05 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,7 +45,7 @@ export async function middleware(req: NextRequest) {
           httpOnly: true,
           secure: !isDevelopment,
           sameSite: "lax",
-          domain: !isDevelopment ? ".api.job-search-service.ru" : "localhost",
+          domain: !isDevelopment ? ".job-search-service.ru" : "localhost",
           maxAge: 15 * 60, // 15 минут для accessToken
         });
 
@@ -53,7 +53,7 @@ export async function middleware(req: NextRequest) {
           httpOnly: true,
           secure: !isDevelopment,
           sameSite: "lax",
-          domain: !isDevelopment ? ".api.job-search-service.ru" : "localhost",
+          domain: !isDevelopment ? ".job-search-service.ru" : "localhost",
           maxAge: 60 * 24 * 60 * 60, // 60 дней для refreshToken
         });
 
